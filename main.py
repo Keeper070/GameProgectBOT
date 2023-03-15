8dea3b6ea4aa609c1701efb7a91acbd6a652d04d
Keeper070 <99165594+Keeper070@users.noreply.github.com>
2023-03-15 21:14:45 +0400

    default

--- main.py
+++ main.py
@@ -198,7 +198,7 @@ def getWeather(message):
 
 
 
-
+check=False
 
 #Выбор игры (крестик или нолик)
 
@@ -211,34 +211,49 @@ def newgame_command(message: telebot.types.Message):
     if message.text == '/newgame':
         msg = bot.send_message(message.chat.id, text='Выбери за кого будешь играть', reply_markup=markup)
 
+
 @bot.callback_query_handler(func=lambda call: True)
 def step(callback_query: telebot.types.CallbackQuery):
     if callback_query.data == 'x':
        ai = symb.symbol0
        player = symb.symbolX
-       board(callback_query.message,ai,player)
+       check=True
+       board_game(callback_query.message,ai,player,check)
+
        # msg= bot.send_message(callback_query.message.from_user.id,'Отличный выбор!Давай начнем: {}'.format(a))
     elif callback_query.data == 'o':
-        msg = bot.send_message(callback_query.message.chat.id, 'Хммм, лааадно, пусть будет по твоему')
+        ai = symb.symbolX
+        player = symb.symbol0
+        check = False
+        board_game(callback_query.message, ai, player, check)
+
+
 
 bot_send=True
-def make_bot_true():
-    global bot_send
-    bot_send = True
+
 
 def make_bot_false():
     global bot_send
     bot_send = False
 
+def plus_hod():
+    hod += 1
+
 ai=symb.symbol0
 player=symb.symbolX
 
 #Игровое поле
-def board(message,ai,player):
+def board_game(message,ai,player,check):
+    global board,hod
     board = [[symb.symbolDef] * 3 for i in range(3)]
     hod = 0
     keyboard = types.InlineKeyboardMarkup(row_width=3)
-    b1 = types.InlineKeyboardButton(text=symb.symbolDef, callback_data=1)
+
+    if(check):
+        b1 = types.InlineKeyboardButton(text=symb.symbolDef, callback_data=1)
+    else:
+        b1 = types.InlineKeyboardButton(text=symb.symbolX, callback_data=1)
+
     b2 = types.InlineKeyboardButton(text=symb.symbolDef, callback_data=2)
     b3 = types.InlineKeyboardButton(text=symb.symbolDef, callback_data=3)
     b4 = types.InlineKeyboardButton(text=symb.symbolDef, callback_data=4)
@@ -250,7 +265,110 @@ def board(message,ai,player):
     keyboard.add(b1, b2, b3, b4, b5, b6, b7, b8, b9)
     if bot_send:
         make_bot_false()
-        mes = bot.send_message(message.chat.id, 'Отличный выбор!Давай, первый ход за тобой', reply_markup=keyboard)
+        if(check):
+            mes = bot.send_message(message.chat.id, 'Отличный выбор!Давай, первый ход за тобой', reply_markup=keyboard)
+        else:
+            msg = bot.send_message(message.chat.id, 'Хммм, лааадно, пусть будет по твоему', reply_markup=keyboard)
+
+@bot.callback_query_handler(func=lambda call: True)
+def query_handler(call: telebot.types.CallbackQuery):
+    if call.data == '1':
+        call_data(0, 0, call.message)
+    elif call.data == '2':
+        call_data(0, 1,call.message)
+    elif call.data == '3':
+        call_data(0, 2, call.message)
+    elif call.data == '4':
+        call_data(1, 0, call.message)
+    elif call.data == '5':
+        call_data(1, 1, call.message)
+    elif call.data == '6':
+        call_data(1, 2, call.message)
+    elif call.data == '7':
+        call_data(2, 0, call.message)
+    elif call.data == '8':
+        call_data(2, 1, call.message)
+    elif call.data == '9':
+        call_data(2, 2, call.message)
+    else:
+        bot.answer_callback_query(call.message.chat.id, 'Игра завершена!')
+
+
+def call_data(index1, index2, message):
+    if hod % 2 != 0:
+        if board[index1][index2] == symb.symbolDef:
+            board[index1][index2] = symb.symbolX
+            plus_hod()
+            bot.send_message(message.chat.id,'Твой ход',reply_markup=make_board())
+    elif hod % 2 != 0:
+        if board[index1][index2] == symb.symbolDef:
+            board[index1][index2] = symb.symbol0
+            plus_hod()
+            bot.send_message(message.chat.id,'Твой ход',reply_markup=make_board())
+
+# def call_data(index1, index2, call):
+#     global board, player
+#     if hod % 2 != 0 or check:
+#         if board[index1][index2] == symb.symbolDef:
+#             board[index1][index2] = symb.symbolX
+#             plus_hod()
+#             bot.edit_message_reply_markup(user2.id, message_id=us2_mes.id, reply_markup=make_board())
+#             bot.edit_message_reply_markup(user1.id, message_id=us1_mes.id, reply_markup=make_board())
+#             if check_win(board)[0] == True or check_win(board)[0] == None:
+#                 if check_win(board)[0] == None:
+#                     draw = True
+#                 else:
+#                     draw = False
+#                 bot.edit_message_reply_markup(user1.id, message_id=us1_mes.id,
+#                                               reply_markup=make_board(end=True, draw=draw, symbol=check_win(board)))
+#                 bot.edit_message_reply_markup(user2.id, message_id=us2_mes.id,
+#                                               reply_markup=make_board(end=True, draw=draw, symbol=check_win(board)))
+#                 players.clear()
+#         else:
+#             bot.answer_callback_query(callback_query_id=call.id, text='Эта клетка уже занята!')
+#     elif hod % 2 != 0 and call.message.chat.id == user1.id:
+#         if board[index1][index2] == symb.symbolDef:
+#             board[index1][index2] = symb.symbol0
+#             plus_hod()
+#             bot.edit_message_reply_markup(user2.id, message_id=us2_mes.id, reply_markup=make_board())
+#             bot.edit_message_reply_markup(user1.id, message_id=us1_mes.id, reply_markup=make_board())
+#             if check_win(board)[0] == True or check_win(board)[0] == None:
+#                 if check_win(board)[0] == None:
+#                     draw = True
+#                 else:
+#                     draw = False
+#                 bot.edit_message_reply_markup(user1.id, message_id=us1_mes.id,
+#                                               reply_markup=make_board(end=True, draw=draw, symbol=check_win(board)))
+#                 bot.edit_message_reply_markup(user2.id, message_id=us2_mes.id,
+#                                               reply_markup=make_board(end=True, draw=draw, symbol=check_win(board)))
+#                 players.clear()
+#         else:
+#             bot.answer_callback_query(callback_query_id=call.id, text='Эта клетка уже занята!')
+#     else:
+#         bot.answer_callback_query(callback_query_id=call.id, text='Не твой ход! Ожидай соперника!')
+
+def make_board(end = False, draw = False, symbol = symb.symbolX ):
+    # if end:
+    #     keyboard = types.InlineKeyboardMarkup(row_width=3)
+    #     if draw:
+    #         txt = 'Конец! Ничья!'
+    #     else:
+    #         txt = f'Конец! Победил игрок , который играл за "{symbol[1]}"'
+    #     b1 = types.InlineKeyboardButton(text=txt, callback_data=102)
+    #     keyboard.add(b1)
+    #     return keyboard
+    keyboard = types.InlineKeyboardMarkup(row_width=3)
+    b1 = types.InlineKeyboardButton(text=board[0][0], callback_data=1)
+    b2 = types.InlineKeyboardButton(text=board[0][1], callback_data=2)
+    b3 = types.InlineKeyboardButton(text=board[0][2], callback_data=3)
+    b4 = types.InlineKeyboardButton(text=board[1][0], callback_data=4)
+    b5 = types.InlineKeyboardButton(text=board[1][1], callback_data=5)
+    b6 = types.InlineKeyboardButton(text=board[1][2], callback_data=6)
+    b7 = types.InlineKeyboardButton(text=board[2][0], callback_data=7)
+    b8 = types.InlineKeyboardButton(text=board[2][1], callback_data=8)
+    b9 = types.InlineKeyboardButton(text=board[2][2], callback_data=9)
+    keyboard.add(b1, b2, b3, b4, b5, b6, b7, b8, b9)
+    return keyboard
 
 #Просмотр побед
 def check_win(board):
@@ -269,53 +387,6 @@ def check_win(board):
         1] != symb.symbolDef and board[1][2] != symb.symbolDef and board[2][0] != symb.symbolDef and board[2][1] != symb.symbolDef and board[2][2] != symb.symbolDef:
         return None, None
     return False, False
-
-
-def plus_hod():
-    global hod
-    hod += 1
-
-def call_data(index1, index2, call):
-    global board, ai,player
-    if hod % 2 == 0 and call.message.chat.id == user2.id:
-        if board[index1][index2] == '◻':
-            board[index1][index2] = 'x'
-            plus_hod()
-            bot.edit_message_reply_markup(user2.id, message_id=us2_mes.id, reply_markup=make_board())
-            bot.edit_message_reply_markup(user1.id, message_id=us1_mes.id, reply_markup=make_board())
-            if check_win(board)[0] == True or check_win(board)[0] == None:
-                if check_win(board)[0] == None:
-                    draw = True
-                else:
-                    draw = False
-                bot.edit_message_reply_markup(user1.id, message_id=us1_mes.id,
-                                              reply_markup=make_board(end=True, draw=draw, symbol=check_win(board)))
-                bot.edit_message_reply_markup(user2.id, message_id=us2_mes.id,
-                                              reply_markup=make_board(end=True, draw=draw, symbol=check_win(board)))
-                players.clear()
-        else:
-            bot.answer_callback_query(callback_query_id=call.id, text='Эта клетка уже занята!')
-    elif hod % 2 != 0 and call.message.chat.id == user1.id:
-        if board[index1][index2] == '◻':
-            board[index1][index2] = 'o'
-            plus_hod()
-            bot.edit_message_reply_markup(user2.id, message_id=us2_mes.id, reply_markup=make_board())
-            bot.edit_message_reply_markup(user1.id, message_id=us1_mes.id, reply_markup=make_board())
-            if check_win(board)[0] == True or check_win(board)[0] == None:
-                if check_win(board)[0] == None:
-                    draw = True
-                else:
-                    draw = False
-                bot.edit_message_reply_markup(user1.id, message_id=us1_mes.id,
-                                              reply_markup=make_board(end=True, draw=draw, symbol=check_win(board)))
-                bot.edit_message_reply_markup(user2.id, message_id=us2_mes.id,
-                                              reply_markup=make_board(end=True, draw=draw, symbol=check_win(board)))
-                players.clear()
-        else:
-            bot.answer_callback_query(callback_query_id=call.id, text='Эта клетка уже занята!')
-    else:
-        bot.answer_callback_query(callback_query_id=call.id, text='Не твой ход! Ожидай соперника!')
-
 
 
 
